d4fcf9c760c7ec6f4eadf0915fe4bb4d7815bf51
Hwanghuntae <ghkdgnsxo2484@naver.com>
2024-05-14 17:26:18 +0900

    functionalization and logic test

--- byslim_list.py
+++ byslim_list.py
@@ -3,41 +3,65 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import chromedriver_autoinstaller
 
-url = "https://www.byslim.com/category/top/6/"
+main_url = "https://www.byslim.com/category/top/6/"
 
 
 chromedriver_autoinstaller.install()
 driver = webdriver.Chrome()
 
+name_list, img_list, link_list, size_list = [], [], [], []
 
-name_list, img_list, link_list = [], [], []
+def get_list(url):
+  driver.get(url)
+  driver.implicitly_wait(time_to_wait=5)
+  page = driver.page_source
+  soup = BeautifulSoup(page, "html.parser")
+  list_page = soup.select(".prdList.grid3")
 
-driver.get(url)
-driver.implicitly_wait(time_to_wait=5)
-page = driver.page_source
-soup = BeautifulSoup(page, "html.parser")
+  for li in list_page:
+    divs = li.find_all(attrs={'class': "thumbnail"})
+    for div in divs:
+      prd_div = div.find('div', class_='prdImg')
+      if prd_div:
+        link_list.append("https://www.byslim.com/" + prd_div.find('a')['href'])
+        name_list.append(prd_div.find('img')['alt'])
+        img_list.append(prd_div.find('img')['src'])
+  
+  return "complete getting list data"
 
-sizes = soup.select(".prdList.grid3")
+def get_size(link_list):
+  url = link_list
+  driver.get(url)
+  driver.implicitly_wait(time_to_wait=5)
+  page = driver.page_source
+  soup = BeautifulSoup(page, "html.parser")
+
+  td_tags = soup.find_all("td")
+  for td in td_tags:
+      if 'style' in td.attrs:
+          if td.text.strip()[0] == "착":
+              break 
+          else: 
+              size_list.append(td.text.strip())  
+  
+  return "complete getting size data"
+              
 
 
-for size in sizes:
-  divs = size.find_all(attrs={'class': "thumbnail"})
-  for div in divs:
-    prd_div = div.find('div', class_='prdImg')
-    if prd_div:
-      link_list.append(prd_div.find('a')['href'])
-      name_list.append(prd_div.find('img')['alt'])
-      img_list.append(prd_div.find('img')['src'])
-    # link = div.attrs["href"]
-    # link_list.append("https://www.byslim.com/" + link)
+print(get_list(main_url))
+print()
+print(name_list[0])
+print(img_list[0])
+print(link_list[0])
+print()
+print(get_size(link_list[0]))
+print(size_list)
 
-    # imgs = div.find('img')
-    
-    # img = imgs["src"]
-    # img_list.append(imgs)
 
-    # name = imgs["alt"]
-    # name_list.append(name)
-    
 
-print(name_list)
+
+
+
+
+
+
